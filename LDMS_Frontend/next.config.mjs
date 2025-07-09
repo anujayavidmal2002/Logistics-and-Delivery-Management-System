d8d3ae06b4269eefb514843823adb83c22f28308
anujayavidmal2002 <vidmalssaa.22@uom.lk>
2025-07-09 20:24:58 +0530

    Fix: updated theme configuration and error handling

--- LDMS_Frontend/next.config.mjs
+++ LDMS_Frontend/next.config.mjs
@@ -9,10 +9,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  allowedDevOrigins: [
-    "http://192.168.34.228:3000", // ← Allow access from this IP
-    "http://localhost:3000", // ← Optional, still allow localhost too
-  ],
+  allowedDevOrigins: ["http://192.168.34.228:3000", "http://localhost:3000"],
 };
 
-export default nextConfig
+export default nextConfig;
